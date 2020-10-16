712fba543a93979d0272e7c45d487ad9c52ebe6b
BarefootITGuy <71440168+BarefootITGuy@users.noreply.github.com>
2020-10-16 06:35:39 -0500

    2nd commit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,12 @@ export default function App() {
         This is a template auto created in seconds using templates on
         codesandbox.io{" "}
       </h2>
+
+      <ul>
+        <li> First item </li>
+        <li> Second item </li>
+        <li> 3rd item </li>
+      </ul>
     </div>
   );
 }
