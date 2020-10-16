2538d0a6596d40cb384f0a2e9b331bcf7c76e255
BarefootITGuy <71440168+BarefootITGuy@users.noreply.github.com>
2020-10-16 06:32:21 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello World! </h1>
+      <h2>
+        {" "}
+        This is a template auto created in seconds using templates on
+        codesandbox.io{" "}
+      </h2>
+    </div>
+  );
+}
